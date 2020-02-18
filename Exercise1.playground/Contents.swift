cd83190661e125817639adf49f94be742c216169
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2020-02-17 19:58:48 -0600

    Solved first exercise
    TODO: Move it to a CLI App for Swift

--- Exercise1.playground/Contents.swift
+++ Exercise1.playground/Contents.swift
@@ -1,3 +1,34 @@
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
+protocol MyArrayInterface {
+    func convertToDynamicArray(a: [String])
+    func clearIndex(index: Int)
+    func obtainArray() -> [String];
+}
+
+class MyDynamicArray {
+    private var myArray : [String]
+    
+    init() {
+        myArray = [String]()
+    }
+}
+
+extension MyDynamicArray : MyArrayInterface {
+    
+    func convertToDynamicArray(a: [String]) {
+        a.enumerated().map({(index, value) in
+            print("The element \(value) has been added at index \(index)")
+            myArray.append(value)
+        })
+    }
+    
+    func clearIndex(index: Int) {
+        print("The element \(myArray[index]) has been converted into null")
+        myArray[index] = ""
+    }
+    
+    func obtainArray() -> [String] {
+        return myArray.filter({!$0.isEmpty})
+    }
+}
